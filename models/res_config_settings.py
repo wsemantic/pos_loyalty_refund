fde60327082466181c79c5c5d571739560a2bbf2
Jay <jay.kotak2021@gmail.com>
2024-10-21 10:09:41 +0530

    Added Second print with price functionality

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -7,3 +7,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     gift_card_product_id = fields.Many2one('product.product', string='Gift Card Product', related='pos_config_id.gift_card_product_id', readonly=False)
+    enable_second_print_with_price = fields.Boolean('Enable Second Print With Price', related='pos_config_id.enable_second_print_with_price', readonly=False)
--- static/src/js/ReceiptScreen.js
+++ static/src/js/ReceiptScreen.js
@@ -21,6 +21,19 @@ odoo.define('pos_loyalty_refund.ReceiptScreen', function (require) {
                 this.printReceipt();
             }, 50);
         }
+        async printReceipt() {
+            const currentOrder = this.currentOrder;
+            const isPrinted = await this._printReceipt();
+            if((this.props.receiptWithoutPrice || this.orderUiState.printGift) && this.env.pos.config.enable_second_print_with_price){
+                $('.current-order-gift-receipt').html($('.pos-receipt-container').html());
+                $('.pos-receipt-container').html($('.current-order-receipt').html());
+                await this._printReceipt();
+                $('.pos-receipt-container').html($('.current-order-gift-receipt').html());
+            }
+            if (isPrinted) {
+                currentOrder._printed = true;
+            }
+        }
     }
 
     Registries.Component.extend(ReceiptScreen, GiftReceiptScreen);
