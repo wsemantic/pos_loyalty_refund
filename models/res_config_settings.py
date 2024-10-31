b0007104196e48b685708b957b5ecccff9a20962
Alfonso Gonzalez <info@wsemantic.com>
2024-10-31 12:33:57 +0100

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -8,3 +8,9 @@ class ResConfigSettings(models.TransientModel):
 
     gift_card_product_id = fields.Many2one('product.product', string='Gift Card Product', related='pos_config_id.gift_card_product_id', readonly=False)
     enable_second_print_with_price = fields.Boolean('Enable Second Print With Price', related='pos_config_id.enable_second_print_with_price', readonly=False)
+    receipt_width = fields.Integer(
+        string="Ancho del Recibo (px)",
+        related="pos_config_id.receipt_width",
+        readonly=False,  # Permite editar el campo desde `res.config.settings`
+        help="300 para impresoras de 80 mm. Modificalo a 220 para impresora de 58mm"
+    )
