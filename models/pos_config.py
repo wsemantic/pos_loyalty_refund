64bdfe55ab45a8187d4daf5746bd0d5b45bc3809
Alfonso Gonzalez <info@wsemantic.com>
2024-10-31 09:49:19 +0100

    v2

--- models/pos_config.py
+++ models/pos_config.py
@@ -7,3 +7,4 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     gift_card_product_id = fields.Many2one('product.product', 'Gift Card Product')
+    enable_second_print_with_price = fields.Boolean('Enable Second Print With Price')
