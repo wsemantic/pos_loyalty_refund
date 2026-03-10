c65147b685b674727392f4bd103261220cfc7e7a
Alfonso Gonzalez <info@wsemantic.com>
2026-03-10 17:44:47 +0100

    [DEBUG] pos receipt barcode: add fallback source and console traces

--- static/src/overrides/components/pos_order.js
+++ static/src/overrides/components/pos_order.js
@@ -1,19 +1,38 @@
 import { PosOrder } from "@point_of_sale/app/models/pos_order";
 import { patch } from "@web/core/utils/patch";
 
+const POS_BARCODE_DEBUG = true;
+
+const debugBarcode = (...args) => {
+    if (POS_BARCODE_DEBUG) {
+        console.log("[pos_loyalty_refund][barcode]", ...args);
+    }
+};
+
 patch(PosOrder.prototype, {
     export_for_printing(baseUrl, headerData) {
         const json = super.export_for_printing(...arguments);
         const simplifiedInvoiceNumber =
             this.l10n_es_simplified_invoice_number ||
             json.l10n_es_simplified_invoice_number;
+        const fallbackOrderRef = this.name || this.pos_reference || json.name || json.pos_reference;
 
         json.gift_card_code = this.gift_card_code;
         json.gift_card_balance = this.gift_card_balance;
         json.l10n_es_simplified_invoice_number = simplifiedInvoiceNumber;
+        json.barcode_debug_fallback_ref = fallbackOrderRef;
 
         json.headerData = json.headerData || {};
         json.headerData.l10n_es_simplified_invoice_number = simplifiedInvoiceNumber;
+        json.headerData.barcode_debug_fallback_ref = fallbackOrderRef;
+
+        debugBarcode("export_for_printing payload", {
+            order_uid: this.uid,
+            order_server_id: this.server_id,
+            simplifiedInvoiceNumber,
+            fallbackOrderRef,
+            headerData: json.headerData,
+        });
 
         return json;
     },
--- static/src/overrides/components/ticket_screen.js
+++ static/src/overrides/components/ticket_screen.js
@@ -4,6 +4,14 @@ import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
 import { useTrackedAsync } from "@point_of_sale/app/utils/hooks";
 
+const POS_BARCODE_DEBUG = true;
+
+const debugBarcode = (...args) => {
+    if (POS_BARCODE_DEBUG) {
+        console.log("[pos_loyalty_refund][barcode]", ...args);
+    }
+};
+
 patch(TicketScreen.prototype, {
     setup() {
         super.setup(...arguments);
@@ -13,16 +21,25 @@ patch(TicketScreen.prototype, {
     },
 
     async printG(order) {
+        debugBarcode("printG start", {
+            server_id: order?.server_id,
+            l10n_es_simplified_invoice_number: order?.l10n_es_simplified_invoice_number,
+            name: order?.name,
+            pos_reference: order?.pos_reference,
+        });
+
         if (order && !order.l10n_es_simplified_invoice_number && order.server_id) {
             const [dbOrder] = await this.orm.read(
                 "pos.order",
                 [order.server_id],
-                ["l10n_es_simplified_invoice_number"]
+                ["l10n_es_simplified_invoice_number", "name", "pos_reference"]
             );
             if (dbOrder?.l10n_es_simplified_invoice_number) {
                 order.l10n_es_simplified_invoice_number = dbOrder.l10n_es_simplified_invoice_number;
             }
+            debugBarcode("printG orm.read pos.order", dbOrder);
         }
+
         await this.pos.printReceipt({
             order: order,
             basic: true,
