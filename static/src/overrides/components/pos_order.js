0101200c1f3f7dc159ffd08aa97d01c464bc3c6e
Alfonso Gonzalez <info@wsemantic.com>
2026-03-10 17:55:37 +0100

    [FIX] pos barcode: align with gift-card route and drop order-ref fallback

--- static/src/overrides/components/pos_order.js
+++ static/src/overrides/components/pos_order.js
@@ -15,22 +15,18 @@ patch(PosOrder.prototype, {
         const simplifiedInvoiceNumber =
             this.l10n_es_simplified_invoice_number ||
             json.l10n_es_simplified_invoice_number;
-        const fallbackOrderRef = this.name || this.pos_reference || json.name || json.pos_reference;
 
         json.gift_card_code = this.gift_card_code;
         json.gift_card_balance = this.gift_card_balance;
         json.l10n_es_simplified_invoice_number = simplifiedInvoiceNumber;
-        json.barcode_debug_fallback_ref = fallbackOrderRef;
 
         json.headerData = json.headerData || {};
         json.headerData.l10n_es_simplified_invoice_number = simplifiedInvoiceNumber;
-        json.headerData.barcode_debug_fallback_ref = fallbackOrderRef;
 
         debugBarcode("export_for_printing payload", {
             order_uid: this.uid,
             order_server_id: this.server_id,
             simplifiedInvoiceNumber,
-            fallbackOrderRef,
             headerData: json.headerData,
         });
 
